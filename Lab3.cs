e91552e8ba55b033b254306f052a8590efe07e2c
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2023-10-24 10:43:43 +0300

    Add files via upload

---
+++ Lab3.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите a,b,c,d,e");
+int chet = 0;
+for (int i = 1; i <= 5; i++) {
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n % 2 == 0) {
+        chet++;
+    }
+}
+Console.WriteLine($"Четные числа - {chet}");
